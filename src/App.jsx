3d0c103605bfa070dcb31ebbd039d324488cee9d
Evgenii Lepekho <lepehun@mail.ru>
2024-05-07 11:54:13 +0200

    feat: add some else info to stepsData

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import StepView from "./components/StepView";
 import { stepsData } from "./stepsData";
 
 const App = () => {
-  const [step, setStep] = useState(0);
+  const [step, setStep] = useState(0); // нынешний шаг
+  const [track, setTrack] = useState([]); // массив предыдущих шагов, можно будет использовать для некоего "дневника истории"
+
+  useEffect(() => {
+    setTrack([...track, step]);
+    console.log("Все предыдущие шаги: ", track);
+    console.log("Шаг: ", step);
+  }, [step]);
 
   const handleOptionClick = (nextStep) => {
     setStep(nextStep);
@@ -13,7 +20,6 @@ const App = () => {
   return (
     <main>
       <header className="header">
-        <p>header</p>
         {/* <h1>In the flames</h1> */}
         {/* <button type="button" onClick={() => setStep(0)}>
           Вернуться к началу
